02cef05c93c55088c9c6d21ce0ae1add75661369
delia <deliag.constantinescu@yahoo.com>
2023-12-20 16:38:15 +0200

    workshop 4 progress
    Signed-off-by: delia <deliag.constantinescu@yahoo.com>

--- src/main/java/com/adobe/devcamp/repository/UserRepository.java
+++ src/main/java/com/adobe/devcamp/repository/UserRepository.java
@@ -1,53 +1,17 @@
 package com.adobe.devcamp.repository;
 
 import com.adobe.devcamp.dto.UserDTO;
+import com.adobe.devcamp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-// TODOs: - unmock this repository
+// done: - unmock this repository
 //        - define some JPA queries
 //        - what's Optional?
 
 @Component
-public class UserRepository implements GenericRepository<UserDTO> {
-    private List<UserDTO> storedUsers;
-
-    public UserRepository() {
-        this.storedUsers = new ArrayList<>();
-    }
-
-    @Override
-    public UserDTO save(UserDTO entity) {
-        storedUsers.add(entity);
-        return entity;
-    }
-
-    @Override
-    public Optional<UserDTO> findById(String id) {
-        for (UserDTO userDTO : storedUsers) {
-            if (userDTO.getId().equals(id)) {
-                return Optional.of(userDTO);
-            }
-        }
-        return Optional.empty();
-    }
-
-    @Override
-    public List<UserDTO> findAll() {
-        return storedUsers;
-    }
-
-    @Override
-    public void deleteById(String id) {
-        // done
-        for (UserDTO u : storedUsers) {
-            if (u.getId().equals(id)) {
-                storedUsers.remove(u);
-                break;
-            }
-        }
-    }
-}
+public interface UserRepository extends JpaRepository<User, String> { }
--- src/main/java/com/adobe/devcamp/service/UserService.java
+++ src/main/java/com/adobe/devcamp/service/UserService.java
@@ -1,12 +1,16 @@
 package com.adobe.devcamp.service;
 
 import com.adobe.devcamp.dto.UserDTO;
+import com.adobe.devcamp.model.Gender;
+import com.adobe.devcamp.model.User;
 import com.adobe.devcamp.repository.UserRepository;
+import com.adobe.devcamp.utils.DTOFactory;
+import com.adobe.devcamp.utils.EntityFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,24 +21,43 @@ public class UserService {
     //    3. Fetching all users
     //    4. Deleting a user
     private final UserRepository userRepository;
+    private final EntityFactory entityFactory;
+    private final DTOFactory dtoFactory;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, EntityFactory entityFactory, DTOFactory dtoFactory) {
         this.userRepository = userRepository;
+        this.entityFactory = entityFactory;
+        this.dtoFactory = dtoFactory;
     }
 
     public UserDTO getUserById(String id) {
-        return null;
+        // done
+        return userRepository.findById(id)
+                .map(dtoFactory::userEntityToDTO)
+                .get();
     }
 
     public List<UserDTO> getAllUsers() {
-        return userRepository.findAll();
+        // done
+        return userRepository.findAll()
+                .stream().map(dtoFactory::userEntityToDTO)
+                .collect(java.util.stream.Collectors.toList());
     }
 
     public UserDTO createUser(UserDTO userDTO) {
-        // TODO: set id
-        userRepository.save(userDTO);
-        return userDTO;
+        // done
+        final User savedUser = userRepository.save(entityFactory.userDTOToEntity(userDTO));
+        return dtoFactory.userEntityToDTO(savedUser);
     }
 
+    public void deleteUserById(String userId) {
+        // done
+        userRepository.deleteById(userId);
+    }
+
+    public List<UserDTO> getAllFemaleUsersYoungerThan30() {
+        // TODO(streams + JPA)x: Implement this method
+        return null;
+    }
 }
